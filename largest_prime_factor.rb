3b29105d9b4f5f82cae0bc77ea980051609fb930
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-07-15 17:52:03 -0500

    Add good practices to my code

--- largest_prime_factor.rb
+++ largest_prime_factor.rb
@@ -1,8 +1,13 @@
-def factor(number = 500)
+=begin
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+=end
+def factor(number=500)
   array = []
   multi = 1
-  (2..number/2).each do |x| 
-    if number % x == 0 
+  (2..number/2).each do |x|
+    if number % x == 0
       multi *= x
       largest = x
       number /= x
