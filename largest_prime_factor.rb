f9a3077310911d67a5270056b251b0ed0d983069
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-07-15 15:46:35 -0500

    I created the varible multi that multiplies the multiplos and in the end gives me 600851475143

---
+++ largest_prime_factor.rb
@@ -0,0 +1,13 @@
+def factor(number = 500)
+  array = []
+  multi = 1
+  (2..number/2).each do |x| 
+    if number % x == 0 
+      puts multi *= x
+      @largest = x
+      number /= x
+    end
+  end
+  @largest
+end
+puts factor(600851475143)
