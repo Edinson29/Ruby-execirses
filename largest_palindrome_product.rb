78918ce63758ea9b06784ed5a0484c4098251080
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-07-16 15:40:38 -0500

    I removed the array called multipliers because it had no use.

--- largest_palindrome_product.rb
+++ largest_palindrome_product.rb
@@ -2,23 +2,22 @@
 A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 
 Find the largest palindrome made from the product of two 3-digit numbers.
-=end  
-end
+=end
 def palindromo
-  multipliers = []
-  (100..999).each do |x|
-    (100..999).each do |j|
-      variable = x*j
+  (100..999).each do |position|
+    (100..999).each do |position2|
+      product = position*position2
       #Here i converted the whole to array
-      numberArray = variable.to_s.chars.map(&:to_i)
+      numberArray = product.to_s.chars.map(&:to_i)
       arrayInvert = numberArray.reverse
       next unless numberArray == arrayInvert
-      @variable = variable2
-      multipliers.push(x,j)
+      @palindrome = numberArray
+      @position = position
+      @position2 = position2
     end
   end
   #return the array to integer
-  print multipliers
-  variable = @variable.join.to_i
+  puts "#{@position} #{@position2}"
+  palindrome = @palindrome.join.to_i
 end
 puts palindromo
