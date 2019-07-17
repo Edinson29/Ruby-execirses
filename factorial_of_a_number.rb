cdf15ac90888974387b0fd1f76642532ce28eedd
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-07-17 17:22:13 -0500

    Here is my first version

---
+++ factorial_of_a_number.rb
@@ -0,0 +1,7 @@
+#As an engineer I must create a method that receives any number and returns the factorial of that number
+def factorial_of_a_number(num=0)
+  factorial = 1
+  1.upto(num) {|iterator| factorial *= iterator}
+  factorial
+end
+puts factorial_of_a_number(5)
