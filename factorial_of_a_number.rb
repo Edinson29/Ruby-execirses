94605a6b647ac7db847810e21ae21bb730a61ec3
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-07-17 17:44:14 -0500

    This algorithm prints the factorial of a number

--- factorial_of_a_number.rb
+++ factorial_of_a_number.rb
@@ -1,4 +1,4 @@
-#As an engineer I must create a method that receives any number and returns the factorial of that number
+#As an engineer I must create a method that receives any number and returns the factorial of that number.
 def factorial_of_a_number(num=0)
   factorial = 1
   1.upto(num) {|iterator| factorial *= iterator}
