eced077978b5c790477a1af29a6df9956be4a5ad
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-07-18 10:46:34 -0500

    optimize the code using a method called inject

--- factorial_of_a_number.rb
+++ factorial_of_a_number.rb
@@ -1,7 +1,5 @@
 #As an engineer I must create a method that receives any number and returns the factorial of that number.
 def factorial_of_a_number(num=0)
-  factorial = 1
-  1.upto(num) {|iterator| factorial *= iterator}
-  factorial
+(1..num).inject(1) { |product, n| product * n }
 end
 puts factorial_of_a_number(5)
