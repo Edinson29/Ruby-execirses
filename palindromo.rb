0783664133a0b33abc0ec55032324cd2042ebc09
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-07-18 15:28:27 -0500

    true or false return according to the word received

--- palindromo.rb
+++ palindromo.rb
@@ -1,6 +1,6 @@
 # Create a method that receives a word and returns whether it is a palindrom or not
 
 def palindromo(word)
-  word == word.reverse ? 'Es un polindromo' : 'No es un polindromo'
+  word == word.reverse ? true : false
 end
-puts palindromo('butifarra')
+puts palindromo('asa')
