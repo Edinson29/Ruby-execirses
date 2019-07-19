f322f69354a52d322283b6a0878259010f9b1796
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-07-19 12:06:07 -0500

    remove the variable that reassigned the value to the (respuest).

--- sum_square_difference.rb
+++ sum_square_difference.rb
@@ -11,7 +11,7 @@ end
 def square_of_the_sum(num = 1)
   respuest = 0
   (1..num).inject { |result, n| respuest = result + n }
-  respues = respuest ** 2
+  respuest ** 2
 end
 def square_differentiator
   square_of_the_sum(100) - sum_of_the_squares(100)
