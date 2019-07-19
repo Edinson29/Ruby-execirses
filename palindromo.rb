324ef9760012f4984569b68ed1ff623f69342a35
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-07-19 10:06:16 -0500

    remove the ternary operator leaving the relational operator (==)

--- palindromo.rb
+++ palindromo.rb
@@ -1,6 +1,6 @@
 # Create a method that receives a word and returns whether it is a palindrom or not
 
 def palindromo(word)
-  word == word.reverse ? true : false
+  word == word.reverse
 end
-puts palindromo('asa')
+puts palindromo('oso')
