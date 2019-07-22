38120b367583f49fb05e69106a3af11e2aa2dd18
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-07-22 14:31:01 -0500

    change the name of the variables according to the ruby style guide

--- 10001st_prime.rb
+++ 10001st_prime.rb
@@ -1,16 +1,15 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 # What is the 10 001st prime number?
 
-def prime(prime = 0)    
-  counter = 2
-  finisher = 4
-  arrayPrime = [2, 3]
-  until arrayPrime.length == prime
-    if arrayPrime.all? { |num| finisher % num != 0 }
-      arrayPrime << finisher
+def prime(prime = 0)
+  top = 4
+  array_prime = [2, 3]
+  until array_prime.length == prime
+    if array_prime.all? { |num| top % num != 0 }
+      array_prime << top
     end
-    finisher +=1 
+    top +=1 
   end
-  arrayPrime.last
+  array_prime.last
 end
 puts prime(10001)
