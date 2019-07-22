3b62635cf8cd6f02d6aa13ed28084af6497fe5ad
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-07-22 11:46:53 -0500

    I made an array to go saving the first numbers and to go dividing the other numbers between these to see if they are prime numbers or not.

--- 10001st_prime.rb
+++ 10001st_prime.rb
@@ -1,8 +1,16 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 # What is the 10 001st prime number?
 
-require 'prime'
-def prime(prime=1)
-  Prime.first(prime)
+def prime(prime = 0)    
+  counter = 2
+  finisher = 4
+  arrayPrime = [2, 3]
+  until arrayPrime.length == prime
+    if arrayPrime.all? { |num| finisher % num != 0 }
+      arrayPrime << finisher
+    end
+    finisher +=1 
+  end
+  arrayPrime.last
 end
 puts prime(10001)
