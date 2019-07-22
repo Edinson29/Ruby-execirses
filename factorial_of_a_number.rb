90638c505f1f7cc54ec9613e1aad33c0e24ee0a4
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-07-22 17:48:23 -0500

    factorial of a number exercise passed to class

--- factorial_of_a_number.rb
+++ factorial_of_a_number.rb
@@ -1,5 +1,8 @@
 #As an engineer I must create a method that receives any number and returns the factorial of that number.
-def factorial_of_a_number(num=0)
-(1..num).inject(1) { |product, n| product * n }
+class Factorial
+  def factorial_of_a_number(num=0)
+  (1..num).inject(1) { |product, n| product * n }
+  end
 end
-puts factorial_of_a_number(5)
+factorial = Factorial.new
+puts factorial.factorial_of_a_number(5)
