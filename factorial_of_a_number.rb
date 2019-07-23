ec7a7628b0c399265ac45ba6fda97b777abf2de0
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-07-23 15:29:12 -0500

    rename the method of factorial_of_a_number.rb

--- factorial_of_a_number.rb
+++ factorial_of_a_number.rb
@@ -1,9 +1,9 @@
 #As an engineer I must create a method that receives any number and returns the factorial of that number.
 
 class Factorial
-  def number(num = 0)
+  def calculate(num = 0)
   (1..num).inject(1) { |product, n| product * n }
   end
 end
 factorial = Factorial.new
-puts factorial.number(5)
+puts factorial.calculate(5)
