e4ea0b53bbd79c2337ce8199003806c91667c213
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-07-23 17:09:40 -0500

    I created the is_prime? method that makes it easier for me to find the largest prime factor.

--- largest_prime_factor.rb
+++ largest_prime_factor.rb
@@ -2,16 +2,23 @@
 # What is the largest prime factor of the number 600851475143 ?
 
 class LargestPrimeFactor
-  def factor(number = 0)
-    multi, largest = 1, nil
-    (2..number/2).each do |x|
-      next unless number % x == 0 
-      largest = x
-      multi *= x
-      number /= x
-    end
-    largest
+  def is_prime?(number = 0)     
+    tests = Array (2..Math.sqrt(number))
+    tests = tests.select { |x| number % x == 0 } 
+    tests.empty? ? true : false
   end
+  def factor(number = 0)
+    counter = 2
+    prime = 0
+    while counter <= number
+      if (number % counter == 0) && is_prime?(counter)
+        prime = counter
+        number /= counter
+      end
+      counter+=1
+    end
+    prime
+    end
 end
 largest_pf = LargestPrimeFactor.new
 puts largest_pf.factor(600851475143)
