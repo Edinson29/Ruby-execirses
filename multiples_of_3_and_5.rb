1618c9b511ecb8e73839469239e22744d79e43e8
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-07-23 15:08:37 -0500

    rename the class and the method of multiples_of_3_and_5.rb and create a parameter for the method calculate

--- multiples_of_3_and_5.rb
+++ multiples_of_3_and_5.rb
@@ -1,16 +1,14 @@
-=begin
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
 
-Find the sum of all the multiples of 3 or 5 below 1000.
-=end
-class Multiples_Of3_And5
-  def multi
+class MultiplesOf3And5
+  def calculate(number = 0)
     multiple = 0
-    1000.times do |counter|
+    number.times do |counter|
       multiple += counter if counter % 3 == 0 || counter % 5 == 0
     end
     multiple 
   end
 end
-multiples = Multiples_Of3_And5.new
-puts multiples.multi
+multiples = MultiplesOf3And5.new
+puts multiples.calculate(1000)
