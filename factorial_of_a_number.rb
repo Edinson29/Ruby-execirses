c1cecd232a13a13228004289db3543cfaf085a34
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-07-29 16:09:02 -0500

    add the metehod initialize

--- factorial_of_a_number.rb
+++ factorial_of_a_number.rb
@@ -2,16 +2,18 @@
 class StringError < StandardError; end
 
 class Factorial
+  def initialize(num = 0)
+    @num = parse_num(num)
+  end
   def parse_num(num)
     raise StringError, "That is not a number" unless is_number?(num)
+    num.to_i
   end
   def is_number?(num)
     num.to_s == num.to_i.to_s
   end
-  def calculate(num = 0)
-    parse_num(num)
-    num = num.to_i
-    num = num.abs
-    (1..num).inject(1) { |product, n| product * n }
+  def calculate
+    @num = @num.abs
+    (1..@num).inject(1) { |product, n| product * n }
   end
 end
--- palindromo.rb
+++ palindromo.rb
@@ -1,7 +1,16 @@
 # Create a method that receives a word and returns whether it is a palindrom or not
+class TypeError < StandardError; end
 
 class LettersPalindromo
+	def is_word?(word)
+    word = word.chars
+    word.all? {|letter| letter.to_s != letter.to_i.to_s}
+  end
+  def op(word)
+    TypeError, 'That is not a word' unless is_word?(word)
+  end
   def palindromo?(word)
+    op(word)
     word == word.reverse
   end
 end
